91d24aa3d8b1ce0d733a49f84536640d17d0e502
comp500 <comp500@users.noreply.github.com>
2018-03-02 17:22:39 +0000

    Add GUI

---
+++ mkwii-presence-java/src/main/java/link/infra/mkwiipresence/MKWiiPresenceWindow.java
@@ -0,0 +1,43 @@
+package link.infra.mkwiipresence;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class MKWiiPresenceWindow {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MKWiiPresenceWindow window = new MKWiiPresenceWindow();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MKWiiPresenceWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
